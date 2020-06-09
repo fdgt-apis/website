c1d079be8f63fb6a742fb9f4f69ede821d40417b
Trezy <tre@trezy.com>
2020-06-09 10:49:32 -0500

    fix(Hero): fix `Getting Started` link

--- components/Hero.js
+++ components/Hero.js
@@ -24,7 +24,7 @@ export const Hero = () => (
 
 			<p><FDGT /> is designed to make the life of Twitch Developers easier. From building Twitch bots, to designing overlays, to developing extensions, we provide the tools you need to build amazing things on Twitch &mdash; without breaking the bank.</p>
 
-			<Link href="/docs/get-started">
+			<Link href="/docs/getting-started">
 				<a className="button">
 					Get Started
 
