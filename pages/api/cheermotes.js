adfdb8573280290149712393134842663eb863d1
Trezy <tre@trezy.com>
2020-08-04 16:33:28 -0500

    improvement(/api/cheermotes): actual error handling

--- pages/api/cheermotes.js
+++ pages/api/cheermotes.js
@@ -12,19 +12,39 @@ const {
 } = process.env
 
 export const handler = async (request, response) => {
-	const {
-		access_token: accessToken,
-	} = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${NEXT_PUBLIC_TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&grant_type=client_credentials`, {
-		method: 'post',
-	}).then(response => response.json())
+	let accessToken = null
+	let cheermotes = null
 
-	// console.log({ accessTokenResponse })
-	const cheermotes = await fetch('https://api.twitch.tv/helix/bits/cheermotes', {
-		headers: {
-			'Client-ID': NEXT_PUBLIC_TWITCH_CLIENT_ID,
-			'Authorization': `Bearer ${accessToken}`,
-		},
-	}).then(response => response.json())
+	try {
+		const response = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${NEXT_PUBLIC_TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&grant_type=client_credentials`, {
+			method: 'post',
+		}).then(response => response.json())
+
+		accessToken = response.access_token
+	} catch (error) {
+		return response.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+			errors: [
+				'Failed to get an access token.',
+				error,
+			],
+		})
+	}
+
+	try {
+		cheermotes = await fetch('https://api.twitch.tv/helix/bits/cheermotes', {
+			headers: {
+				'Client-ID': NEXT_PUBLIC_TWITCH_CLIENT_ID,
+				'Authorization': `Bearer ${accessToken}`,
+			},
+		}).then(response => response.json())
+	} catch (error) {
+		return response.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+			errors: [
+				'Failed to get cheermotes.',
+				error,
+			],
+		})
+	}
 
   response.status(httpStatus.OK).json(cheermotes.data)
 }
