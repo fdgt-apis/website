9d7e9e82fe05e8b1e7890387538773166f49c2fe
Trezy <tre@trezy.com>
2020-08-04 16:24:46 -0500

    chore(/api/cheermotes): remove superfluous ENV var

--- pages/api/cheermotes.js
+++ pages/api/cheermotes.js
@@ -7,21 +7,21 @@ import httpStatus from 'helpers/httpStatus'
 
 
 const {
-	TWITCH_CLIENT_ID,
+	NEXT_PUBLIC_TWITCH_CLIENT_ID,
 	TWITCH_CLIENT_SECRET,
 } = process.env
 
 export const handler = async (request, response) => {
 	const {
 		access_token: accessToken,
-	} = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&grant_type=client_credentials`, {
+	} = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${NEXT_PUBLIC_TWITCH_CLIENT_ID}&client_secret=${TWITCH_CLIENT_SECRET}&grant_type=client_credentials`, {
 		method: 'post',
 	}).then(response => response.json())
 
 	// console.log({ accessTokenResponse })
 	const cheermotes = await fetch('https://api.twitch.tv/helix/bits/cheermotes', {
 		headers: {
-			'Client-ID': TWITCH_CLIENT_ID,
+			'Client-ID': NEXT_PUBLIC_TWITCH_CLIENT_ID,
 			'Authorization': `Bearer ${accessToken}`,
 		},
 	}).then(response => response.json())
