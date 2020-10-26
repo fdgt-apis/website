0c91c9d02e3ff75c663ed80f9ad8599b6dcbe265
Trezy <tre@trezy.com>
2020-10-26 14:26:26 -0500

    add 500 page

---
+++ pages/500.js
@@ -0,0 +1,27 @@
+// Module imports
+import React from 'react'
+
+
+
+
+
+// Local imports
+import { ErrorPage } from 'components/ErrorPage'
+import { getCommands } from 'helpers/getCommands'
+import { PageWrapper } from 'components/PageWrapper'
+
+
+
+
+
+export default props => (
+	<PageWrapper {...props}>
+		<ErrorPage statusCode={500} />
+	</PageWrapper>
+)
+
+
+
+
+
+export const getStaticProps = getCommands
