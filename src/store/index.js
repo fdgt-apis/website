78bfb8054f142170a57fa293dff777c32cdc0c91
Trezy <t@trezy.com>
2022-03-28 15:12:41 -0500

    add Zustand

---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+// Module imports
+import create from 'zustand/vanilla'
+
+
+
+
+
+export const store = create((set, get) => ({
+	isLoggedIn: false,
+
+	async login() {},
+}))
---
+++ src/store/react.js
@@ -0,0 +1,15 @@
+// Module imports
+import create from 'zustand'
+
+
+
+
+
+// Local imports
+import { store } from './index.js'
+
+
+
+
+
+export const useStore = create(store)
